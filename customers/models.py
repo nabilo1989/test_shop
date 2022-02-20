113c624005dfc1445f5b67b3db111873e432ab9d
majidnabilo <nabilo.majid@gmail.com>
2022-02-21 01:46:59 +0330

    initail customer app and create models and form

---
+++ customers/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, full_name, password):
+        if not email:
+            raise ValueError('users must have Email')
+        if not full_name:
+            raise ValueError('users must have Full Name')
+
+        user = self.model(email=self.normalize_email(email), full_name=full_name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, full_name, password):
+        user = self.create_user(email, full_name, password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255, unique=True)
+    phone_number=models.CharField(max_length=13,unique=True)
+    full_name = models.CharField(max_length=100)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = MyUserManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['full_name']
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
